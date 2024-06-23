1dc8382aec0060d1307efdd24f1e61be95c8ab0d
Adam Wawerski <adamw467@gmail.com>
2024-06-23 11:08:23 +0200

    Add print sum

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ shopping_dict = {
 
 for shop, list in shopping_dict.items():
     print(f"Idę do {shop.capitalize()} kupuje tu nstępujące rzeczy: {list}")
+print(f"W sumie kupuje tu {sum([len(item) for item in shopping_dict.values()])} produktów")
 
